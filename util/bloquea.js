fb1ad11908e42c5875d5641e72dc20f242ef9940
raicerk <raicerk@gmail.com>
2019-05-02 10:32:55 -0400

    Se agrega blur para ocultar info segun palabras seleccionadas

--- util/bloquea.js
+++ util/bloquea.js
@@ -1,6 +1,6 @@
 read("ListaPalabras").then(res => {
     for (item in res) {
         let variable = $(":contains('" + res[item] + "'):not(:has(:contains('" + res[item] + "')))");
-        $(variable).attr("style", "color:red !important;");
+        $(variable).attr("style", "filter: blur(4px) !important;");
     }
 })
